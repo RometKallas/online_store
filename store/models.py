4237c017e6162c37d8ced2c5fe7bfa14bc6da332
Romet <romet.kallas@gmail.com>
2021-04-26 09:52:36 +0300

    feature(models): Create Category & Product models, run migrations - CU-ht4r5w

--- store/models.py
+++ store/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+#Model for Product Category
+class Category(models.Model):
+    name = models.CharField(max_length=255, db_index=True)
+    slug = models.SlugField(max_length=255, unique=True)
+
+    #to be able to better reference the data
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    #Linking the product to the category table
+    category = models.ForeignKey(Category, related_name ='product', on_delete=models.CASCADE) #When the referenced object is deleted, also delete the objects that have references to it
+    created_by = models.ForeignKey(User, related_name='product_creator', on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    image = models.ImageField(upload_to='images/')
+    slug = models.SlugField(max_length=255, unique=True)
+    price = models.DecimalField(max_digits=4, decimal_places=2)
+    in_stock = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('-price',)
+
+    def __str__(self):
+        return self.title
