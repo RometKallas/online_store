138524e718242b94193b7c4de3f86a688e3a17d1
Romet <romet.kallas@gmail.com>
2021-04-29 23:46:14 +0300

    Created a product category, added a product and a picture CU-j19htu

--- store/admin.py
+++ store/admin.py
@@ -6,6 +6,7 @@ from .models import Category, Product
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['name', 'slug']
+    #prepoluates the slug with the title
     prepopulated_fields = {'slug': ('name',)}
 
 @admin.register(Product)
@@ -13,5 +14,7 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ['title', 'slug', 'price',
                     'in_stock', 'created', 'updated']
     list_filter = ['in_stock', 'is_active']
+    #to be able to edit directly in the admin panel
     list_editable = ['price', 'in_stock']
+    #prepoluates the slug with the title
     prepopulated_fields = {'slug': ('title',)}
