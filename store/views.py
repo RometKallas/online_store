16716535df8ec9dc352f79c42a5d184ce462825b
Romet <romet.kallas@gmail.com>
2021-05-08 15:39:16 +0300

    Create home view - CU-jfad6x

--- store/views.py
+++ store/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+from .models import Category, Product
 # Create your views here.
+
+def home(request):
+    #Running a query against the product table to collect all data
+    products = Product.objects.all()
+    return render(request, 'store/home.html', {'products': products})
