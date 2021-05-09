f5927ce0f8916185c43dae17ed3d44da6f311976
Romet <romet.kallas@gmail.com>
2021-05-09 22:49:24 +0300

    Add the cart in the base.html file so that it is always available and link it to the basket_summary html. Rename some files

--- basket/views.py
+++ basket/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def basket_summary(request):
-    return render(request, 'store/basket/cart.html')
+    return render(request, 'store/basket/basket_summary.html')
