2a284eaca7e602704f9715732c9e113a64dd3dbf
Romet <romet.kallas@gmail.com>
2021-05-09 12:17:01 +0300

    Create a product page URL, view function and template - CU-jh39nn

--- store/urls.py
+++ store/urls.py
@@ -6,5 +6,7 @@ from . import views
 app_name = 'store'
 
 urlpatterns = [
-    path('', views.homepage, name='homepage')
+    path('', views.homepage, name='homepage'),
+    #first slug is the type of data the second slug is the variable
+    path('item/<slug:slug>/', views.product_detail, name='product_detail'),
 ]
--- store/views.py
+++ store/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+#get object is a shortcut to access data from the database
 from .models import Category, Product
 # Create your views here.
 
@@ -12,3 +12,8 @@ def homepage(request):
     #Running a query against the product table to collect all data
     products = Product.objects.all()
     return render(request, 'store/home.html', {'products': products})
+
+def product_detail(request, slug):
+    #based on the slug it is gettign the product we need to return
+    product = get_object_or_404(Product, slug=slug, in_stock=True)
+    return render(request, 'store/product_page.html', {'product': product})
