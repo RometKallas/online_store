7bdf62ba508f55c2206188ac3c0621e897240a3f
Romet <romet.kallas@gmail.com>
2021-05-14 22:24:48 +0300

    fetch the quantity from the ajax request in views.py and update the session with the quantity in basket.py '' :q

--- basket/basket.py
+++ basket/basket.py
@@ -17,14 +17,14 @@ class Basket():
         #if the session does exist then use the same basket
         self.basket = basket
 
-    def add(self, product):
+    def add(self, product, qty):
         """
         Add or update the session with the basket information
         """
         product_id = product.id
 
         if product_id not in self.basket:
-            self.basket[product_id] = {'price': str(product.price)}
+            self.basket[product_id] = {'price': str(product.price), 'qty': int(qty)}
 
         #we save the information into to the session
         self.session.modified = True
--- basket/views.py
+++ basket/views.py
@@ -15,7 +15,8 @@ def basket_add(request):
     basket = Basket(request)
     if request.POST.get('action') == 'post':
         product_id = int(request.POST.get('productid'))
+        product_qty = int(request.POST.get('productqty'))
         product = get_object_or_404(Product, id=product_id)
-        basket.add(product=product)
+        basket.add(product=product, qty=product_qty)
         response = JsonResponse({'test':'data'})
         return response
