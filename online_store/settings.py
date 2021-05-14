b6329fc1a2fea2d1c24b6a18eb3922aae307eee8
Romet <romet.kallas@gmail.com>
2021-05-14 21:34:58 +0300

    Capture the product id when add to basket button is clicked

--- online_store/settings.py
+++ online_store/settings.py
@@ -65,7 +65,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'store.views.categories'
+                'store.views.categories',
+                'basket.context_processors.basket'
             ],
         },
     },
