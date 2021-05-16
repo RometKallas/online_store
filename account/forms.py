e606dae95916a3d1509bdd61c28f9a7076ef6f2a
Romet <romet.kallas@gmail.com>
2021-05-16 13:38:47 +0300

    Add a login url, point it to an existing view, create a UserLoginForm function and inherit from AuthForm, and create a login template

--- account/forms.py
+++ account/forms.py
@@ -1,6 +1,21 @@
 from django import forms
 from .models import UserBase
 
+from django.contrib.auth.forms import (AuthenticationForm)
+
+#we want to use bootstrap styling that's why override
+class UserLoginForm(AuthenticationForm):
+
+    username = forms.CharField(widget=forms.TextInput(
+        attrs={'class': 'form-control mb-3', 'placeholder': 'Username', 'id': 'login-username'}))
+    password = forms.CharField(widget=forms.PasswordInput(
+        attrs={
+            'class': 'form-control',
+            'placeholder': 'Password',
+            'id': 'login-pwd',
+        }
+    ))
+
 class RegistrationForm(forms.ModelForm):
 
     user_name = forms.CharField(label='Enter Username', min_length=4, max_length=50, help_text='Required')
