15c8d9b37fe985177909f023133db9322356c41b
Romet <romet.kallas@gmail.com>
2021-05-16 08:39:03 +0300

    Create a model for creating a super user

--- account/models.py
+++ account/models.py
@@ -36,3 +36,19 @@ class UserBase(AbstractBaseUser, PermissionsMixin):
         verbose_name = "Accounts"
         verbose_name_plural = "Accounts"
 
+class CustomAccountManager(BaseUserManager):
+
+    def create_superuser(self, email, user_name, password, **other_fields):
+
+        other_fields.setdefault('is_staff', True)
+        other_fields.setdefault('is_superuser', True)
+        other_fields.setdefault('is_active', True)
+
+        if other_fields.get('is_staff') is not True:
+            raise ValueError(
+                'Superuser must be assigned to is_staff=True.')
+        if other_fields.get('is_superuser') is not True:
+            raise ValueError(
+                'Superuser must be assigned to is_superuser=True.')
+
+        return self.create_user(email, user_name, password, **other_fields)
