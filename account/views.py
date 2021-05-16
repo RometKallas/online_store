82872e0a2562149020e657dc9fd799009bc30ddd
Romet <romet.kallas@gmail.com>
2021-05-16 10:40:29 +0300

    fix some indention errors and a typo in the forms class

--- account/views.py
+++ account/views.py
@@ -8,9 +8,9 @@ def account_register(request):
     if request.user.is_authenticated:
         return redirect('/')
 
-     if request.method == 'POST':
-         registerForm = RegistrationForm(request.POST)
-         if registerForm.is_valid():
+    if request.method == 'POST':
+        registerForm = RegistrationForm(request.POST)
+        if registerForm.is_valid():
             user = registerForm.save(commit=False)
             user.email = registerForm.cleaned_data['email']
             user.set_password(registerForm.cleaned_data['password'])
